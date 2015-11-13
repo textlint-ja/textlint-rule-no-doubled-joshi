cca741077e0375b680878f0f2983391c73a92899
azu <azuciao@gmail.com>
2015-11-14 00:18:09 +0900

    refactor(rule): rewrite interval based rule

---
+++ README.md
@@ -0,0 +1,44 @@
+# Project Name
+
+- [ ] Write a project description
+
+## Installation
+
+- [ ] Describe the installation process
+
+## Usage
+
+### Options
+
+```
+{
+    "rules": {
+        "no-doubled-joshi": {
+            "max-count": 2,
+            "interval-of-tokens" : 2
+        }
+    }
+}
+```
+
+
+## Tests
+
+- [ ] Write How to Tests
+
+## Contributing
+
+1. Fork it!
+2. Create your feature branch: `git checkout -b my-new-feature`
+3. Commit your changes: `git commit -am 'Add some feature'`
+4. Push to the branch: `git push origin my-new-feature`
+5. Submit a pull request :D
+
+## License
+
+MIT
+
+## Reference
+
+- [Doubled Joshi Validator · Issue #460 · redpen-cc/redpen](https://github.com/redpen-cc/redpen/issues/460 "Doubled Joshi Validator · Issue #460 · redpen-cc/redpen")
+- [事象の構造から見る二重デ格構文の発生 ](https://www.ninjal.ac.jp/event/specialists/project-meeting/files/JCLWorkshop_no6_papers/JCLWorkshop_No6_01.pdf "JCLWorkshop_No6_01.pdf")
--- src/no-double-joshi.js
+++ src/no-double-joshi.js
@@ -1,42 +1,71 @@
 // LICENSE : MIT
 "use strict";
-import {getTokenizer} from "./kuromojin";
-import kuromojin from "./kuromojin";
-import splitSentences from "./sentence-splitter";
+import {getTokenizer} from "kuromojin";
+import splitSentences, {Syntax as SentenceSyntax} from "sentence-splitter";
+/**
+ * create a object that
+ * map ={
+ *   // these token.surface_form === "Hoge"
+ *  "Hoge" [token, token]
+ * }
+ * @param tokens
+ * @returns {*}
+ */
+function createSurfaceKeyMap(tokens) {
+    return tokens.reduce((keyMap, token) => {
+        // "は" : [token]
+        if (!keyMap[token.surface_form]) {
+            keyMap[token.surface_form] = [];
+        }
+        keyMap[token.surface_form].push(token);
+        return keyMap;
+    }, {});
+}
 export default function (context) {
     let {Syntax, report, getSource, RuleError} = context;
-    let tokenizer = null;
     return {
         [Syntax.Str](node){
             let text = getSource(node);
-            let sentences = splitSentences(text);
+            let sentences = splitSentences(text).filter(node => {
+                return node.type === SentenceSyntax.Sentence;
+            });
             return getTokenizer().then(tokenizer => {
                 sentences.forEach(sentence => {
-                    let tokenCounter = {};
-                    let tokens = tokenizer.tokenize(sentence.raw);
-                    let matchLastToken = null;
-                    tokens.forEach(token => {
-                        if (token.pos === "助詞") {
-                            if (!tokenCounter[token.surface_form]) {
-                                tokenCounter[token.surface_form] = {
-                                    count: 0
-                                };
-                            }
-                            tokenCounter[token.surface_form].count++;
-                            tokenCounter[token.surface_form].matchLastToken = token;
-                        }
+                    let tokens = tokenizer.tokenizeForSentence(sentence.raw);
+                    let joshiTokens = tokens.filter(token => {
+                        return token.pos === "助詞";
                     });
-                    Object.keys(tokenCounter).forEach(key => {
-                        let count = tokenCounter[key].count;
-                        let matchLastToken = tokenCounter[key].matchLastToken;
-                        if (count >= 2) {
-                            report(node, new RuleError(`一文に二回以上利用されている助詞 "${key}" がみつかりました。`, {
-                                line: sentence.loc.start.line - 1,
-                                // matchLastToken.word_position start with 1
-                                // this is padding column start with 0 (== -1)
-                                column: sentence.loc.start.column + (matchLastToken.word_position - 1)
-                            }))
+                    let joshiTokenSurfaceKeyMap = createSurfaceKeyMap(joshiTokens);
+                    /*
+                    # Data Structure
+
+                        joshiTokens = [tokenA, tokenB, tokenC, tokenD, tokenE, tokenF]
+                        joshiTokenSurfaceKeyMap = {
+                            "は": [tokenA, tokenC, tokenE],
+                            "で": [tokenB, tokenD, tokenF]
                         }
+                     */
+                    Object.keys(joshiTokenSurfaceKeyMap).forEach(key => {
+                        let tokens = joshiTokenSurfaceKeyMap[key];
+                        if (tokens.length <= 1) {
+                            return;// no duplicated token
+                        }
+                        // if found differenceIndex less than
+                        tokens.reduce((prev, current) => {
+                            let startPosition = joshiTokens.indexOf(prev);
+                            let otherPosition = joshiTokens.indexOf(current);
+                            // if difference
+                            let differenceIndex = otherPosition - startPosition;
+                            if (differenceIndex <= 2) {
+                                report(node, new RuleError(`一文に二回以上利用されている助詞 "${key}" がみつかりました。`, {
+                                    line: sentence.loc.start.line - 1,
+                                    // matchLastToken.word_position start with 1
+                                    // this is padding column start with 0 (== -1)
+                                    column: sentence.loc.start.column + (current.word_position - 1)
+                                }));
+                            }
+                            return current;
+                        });
                     });
                 });
             });
