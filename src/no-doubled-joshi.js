c6fc90bab3f5c24eb536c67fe2ad4f2919b2d4ce
azu <azuciao@gmail.com>
2016-02-22 10:00:33 +0900

    feat(rule):  異なる種類の助詞の重複を許可する
     助詞の品詞細分類1までを見て助詞同士を比較する。
    
     > ターミナルで「test」**と**入力する**と**、画面に表示されます。
    
     1個目の「と」は格助詞、2個めの「と」は接続助詞となるため、異なるものとして認識しエラーとしない。
    
     fix #6

--- src/no-doubled-joshi.js
+++ src/no-doubled-joshi.js
@@ -5,11 +5,20 @@ import {getTokenizer} from "kuromojin";
 import splitSentences, {Syntax as SentenceSyntax} from "sentence-splitter";
 import StringSource from "textlint-util-to-string";
 // 助詞どうか
-const is助詞Token = token => {
+const is助詞Token = (token) => {
     return token.pos === "助詞";
 };
-const is読点Token = token => {
+const is読点Token = (token) => {
     return token.surface_form === "、" && token.pos === "名詞";
+};
+// 助詞tokenから品詞細分類1までを元にしたkeyを作る
+const createKeyFromKey = (token) => {
+    // e.g.) は:助詞.係助詞
+    return `${token.surface_form}:${token.pos}.${token.pos_detail_1}`
+};
+// keyからsurfaceを取り出す
+const restoreToSurfaceFromKey = (key) => {
+    return key.split(":")[0];
 };
 /**
  * Create token map object
@@ -23,11 +32,12 @@ const is読点Token = token => {
 function createSurfaceKeyMap(tokens) {
     // 助詞のみを対象とする
     return tokens.filter(is助詞Token).reduce((keyMap, token) => {
-        // "は" : [token]
-        if (!keyMap[token.surface_form]) {
-            keyMap[token.surface_form] = [];
+        // "は:助詞.係助詞" : [token]
+        const tokenKey = createKeyFromKey(token);
+        if (!keyMap[tokenKey]) {
+            keyMap[tokenKey] = [];
         }
-        keyMap[token.surface_form].push(token);
+        keyMap[tokenKey].push(token);
         return keyMap;
     }, {});
 }
@@ -100,12 +110,13 @@ export default function (context, options = {}) {
 
                         joshiTokens = [tokenA, tokenB, tokenC, tokenD, tokenE, tokenF]
                         joshiTokenSurfaceKeyMap = {
-                            "は": [tokenA, tokenC, tokenE],
-                            "で": [tokenB, tokenD, tokenF]
+                            "は:助詞.係助詞": [tokenA, tokenC, tokenE],
+                            "で:助詞.係助詞": [tokenB, tokenD, tokenF]
                         }
                      */
                     Object.keys(joshiTokenSurfaceKeyMap).forEach(key => {
-                        let tokens = joshiTokenSurfaceKeyMap[key];
+                        const tokens = joshiTokenSurfaceKeyMap[key];
+                        const joshiName = restoreToSurfaceFromKey(key);
                         // strict mode ではない時例外を除去する
                         if (!isStrict) {
                             if (matchExceptionRule(tokens)) {
@@ -134,7 +145,7 @@ export default function (context, options = {}) {
                                     // this is padding column start with 0 (== -1)
                                     column: originalPosition.column
                                 };
-                                report(node, new RuleError(`一文に二回以上利用されている助詞 "${key}" がみつかりました。`, padding));
+                                report(node, new RuleError(`一文に二回以上利用されている助詞 "${joshiName}" がみつかりました。`, padding));
                             }
                             return current;
                         });
